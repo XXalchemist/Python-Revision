f9460309b7cc3145f57904bb5de2fb52ff02d9cf
Narayan Krishna <nkrishn8@gmail.com>
2020-07-21 19:32:00 +0530

    added Library class functions

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -9,16 +9,24 @@ class Library:
         self.lendDict = {}
     
     def displayBooks(self):
-        pass
+        print( f"We have following books in library: {self.name}" )
+        for book in self.booksList:
+            print(book)
 
-    def lendBook(self):
-        pass
+    def lendBook(self, user, book):
+        if book not in self.lendDict.keys():
+            self.lendDict.update({book : user})
+            print("Database updated .... You can take your book now !!!")
+        else:
+            print( f"Sorry ! This book is already being used by {self.lendDict[book]}")
 
-    def addBook(self):
-        pass
+    def addBook(self, book):
+        self.booksList.append(book)
+        print("Book has been added to the list")
 
     def returnBook(self, book):
-        pass
+        self.booksList.remove(book)
+        print("Book has been removed from the list")
 
 if __name__ == '__main__' :
-    library = Library()
+    library_A = Library()
