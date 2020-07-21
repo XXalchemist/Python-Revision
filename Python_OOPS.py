73afb2c3a38369c5e24ea8ff98cb2de32e963e07
Narayan Krishna <nkrishn8@gmail.com>
2020-07-21 19:23:22 +0530

    Library class added

--- Python_OOPS.py
+++ Python_OOPS.py
@@ -1,3 +1,22 @@
 '''
 Library Implementation through Python OOPS concept
 '''
+
+class Library:
+    def __init__(self, list, name):
+        self.booksList = list
+        self.name = name
+        self.lendDict = {}
+    
+    def displayBooks(self):
+        pass
+
+    def lendBook(self):
+        pass
+
+    def addBook(self):
+        pass
+
+    def returnBook(self, book):
+        pass
+
